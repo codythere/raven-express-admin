1577f9962f4a1a9f62949fcc497aea81fb9ebaec
Cody Lai <130062212+codythere@users.noreply.github.com>
2025-11-25 11:55:49 +0800

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { signOut } from "next-auth/react";
 import Logo from "./Logo";
-// import { useSession } from "next-auth/react";
 import toast from "react-hot-toast";
 
 export default function Nav({ show }) {
@@ -12,8 +11,6 @@ export default function Nav({ show }) {
   const activeIcon = "w-6 h-6 text-primary";
   const router = useRouter();
   const { pathname } = router;
-  // const { data: session } = useSession();
-  // const isAdmin = session?.user?.isAdmin;
 
   async function logout() {
     await router.push("/");
@@ -123,7 +120,6 @@ export default function Nav({ show }) {
           </svg>
           分類
         </Link>
-        // {isAdmin && (
           <Link
             href={"/orders"}
             className={pathname.includes("/orders") ? activeLink : inactiveLink}
@@ -155,41 +151,6 @@ export default function Nav({ show }) {
             )}
             所有訂單
           </Link>
-        // )}
-        // {!isAdmin && (
-        //   <button
-        //     type="button"
-        //     onClick={() => toast.error("只有管理員可以查看所有訂單")}
-        //     className={pathname.includes("/orders") ? activeLink : inactiveLink}
-        //   >
-        //     {pathname.includes("/orders") ? (
-        //       <svg
-        //         xmlns="http://www.w3.org/2000/svg"
-        //         viewBox="0 0 24 24"
-        //         fill="currentColor"
-        //         className={activeIcon}
-        //       >
-        //         <path d="M5.625 3.75a2.625 2.625 0 100 5.25h12.75a2.625 2.625 0 000-5.25H5.625zM3.75 11.25a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5H3.75zM3 15.75a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zM3.75 18.75a.75.75 0 000 1.5h16.5a.75.75 0 000-1.5H3.75z" />
-        //       </svg>
-        //     ) : (
-        //       <svg
-        //         xmlns="http://www.w3.org/2000/svg"
-        //         fill="none"
-        //         viewBox="0 0 24 24"
-        //         strokeWidth={1.5}
-        //         stroke="currentColor"
-        //         className="w-6 h-6"
-        //       >
-        //         <path
-        //           strokeLinecap="round"
-        //           strokeLinejoin="round"
-        //           d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
-        //         />
-        //       </svg>
-        //     )}
-        //     所有訂單
-        //   </button>
-        // )}
         <Link
           href={"/admins"}
           className={pathname.includes("/admins") ? activeLink : inactiveLink}
